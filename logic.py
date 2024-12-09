f2453274274c7fee973d68e766efbb0aba3ea458
alcosoda <bpewds33@gmail.com>
2024-12-09 21:08:01 +0300

    added error-management

--- logic.py
+++ logic.py
@@ -1,16 +1,33 @@
 import requests
 
+
 def get_location_key(api_key, city):
     url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}"
-    response = requests.get(url)
-    data = response.json()
-    return data[0]['Key']
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Проверка на HTTP ошибки
+        data = response.json()
+        if data:
+            return data[0]['Key']
+        else:
+            raise ValueError("Город не найден")
+    except (requests.exceptions.RequestException, ValueError) as e:
+        print(f"Ошибка при получении ключа локации: {e}")
+        return None  # Возвращаем None в случае ошибки
+
 
 def get_weather_forecast(api_key, location_key):
+    if location_key is None:
+        return None
     url = f"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}?apikey={api_key}&metric=true&details=true"
-    response = requests.get(url)
-    forecast_data = response.json()
-    return forecast_data['DailyForecasts'][0]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        forecast_data = response.json()
+        return forecast_data['DailyForecasts'][0]
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при получении прогноза погоды: {e}")
+        return None
 
 def check_bad_weather(forecast):
     temperature = forecast['Temperature']['Maximum']['Value']
