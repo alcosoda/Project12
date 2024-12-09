907b0f1f164d6f4547dd3ca9a1e0dc320e4e8673
alcosoda <bpewds33@gmail.com>
2024-12-09 21:35:58 +0300

    more typeerror managing

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, request
-from logic import get_location_key, get_weather_forecast, check_bad_weather
-import requests
+from flask import Flask, render_template, request, flash
 from flask_caching import Cache
+from logic import get_location_key, get_weather_forecast, check_bad_weather
+import re
+import requests
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'}) # Используем простое кэширование
-
-api_key = "1aVr87Bt3NlBICxPnuqQlqNXGkk4o6Rv"  # Твой API ключ
+app.secret_key = 'your_secret_key'  # Установите свой секретный ключ
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+api_key = "1aVr87Bt3NlBICxPnuqQlqNXGkk4o6Rv"
 
 @app.route("/", methods=["GET", "POST"])
 @cache.cached(timeout=3600)  # Кэшируем результаты на 1 час
 def index():
     if request.method == "POST":
         city = request.form["city"]
+        # Валидация названия города: только буквы и пробелы
+        if not re.match(r"^[a-zA-Zа-яА-ЯёЁ\s]+$", city):
+            flash("Некорректное название города. Используйте только буквы и пробелы.")
+            return render_template("index.html")
         location_key = get_location_key(api_key, city)
         if location_key is None:
             return render_template("error.html", message="Город не найден или произошла ошибка при получении данных.")
