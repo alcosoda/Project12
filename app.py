d15d02bcc8823ad8e62de6b92332411c37263d1c
alcosoda <bpewds33@gmail.com>
2024-12-12 22:29:17 +0300

    Добавил любимый функционал добавления множества варинтов, резалт идеальный, инпут ужасный

--- app.py
+++ app.py
@@ -1,44 +1,35 @@
 from flask import Flask, render_template, request
-
-from logic import Connect, Weather # Импортируем классы из logic.py
+from logic import Connect, Weather
 
 app = Flask(__name__)
+api_key = 'Jul2EgzWSP0sgvNojXxIAbzCEJmMSlrq'  # Replace with your AccuWeather API key
 
-api_key = 'Jul2EgzWSP0sgvNojXxIAbzCEJmMSlrq'
-
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
+    if request.method == 'POST':
+        try:
+            cities = request.form.getlist('cities[]')
+            weather_data = {}
+
+            for city in cities:
+                api = Connect(api_key=api_key)
+                weather_lst = api.get_weather(city)
+
+                weather_data[city] = []
+                for item in weather_lst:
+                    item.info = item.validate()
+                    weather_data[city].append(item)
+
+            return render_template('index.html', weather_data=weather_data, format={'Day': 'День', 'Night': 'Ночь'})
+
+        except KeyError:
+            return render_template('error_message.html', msg='В форме не хватает полей')
+        except IndexError:
+            return render_template('error_message.html', msg='Город не найден')
+        except Exception:
+            return render_template('error_message.html', msg='Не удаётся подключиться к API')
+
     return render_template('input.html')
-
-
-@app.route('/', methods=['POST'])
-def main_page_post():
-    try:
-        form = request.form
-        point_start, point_end = form['point_start'], form['point_end']
-
-        api = Connect(api_key=api_key)
-        start_lst = api.get_weather(point_start)
-        end_lst = api.get_weather(point_end)
-
-        weather_start = [item for item in start_lst]
-        weather_end = [item for item in end_lst]
-
-        for item in weather_start:
-            item.info = item.validate()
-        for item in weather_end:
-            item.info = item.validate()
-
-    except KeyError:
-        return render_template('error_message.html', msg='В форме не хватает полей')
-    except IndexError:
-        return render_template('error_message.html', msg='Город не найден')
-    except Exception:
-        return render_template('error_message.html', msg='Не удаётся подключиться к API')
-    return render_template('index.html', start=weather_start, end=weather_end,
-                           format={'Day': 'День', 'Night': 'Ночь'})
-
 
 if __name__ == '__main__':
     app.run()
